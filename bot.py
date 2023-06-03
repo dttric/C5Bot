6c7f3600062ea609f3c0ab38ef4c23ef7cf4a1d5
dttric <chudinovd222@gmail.com>
2023-06-03 20:30:34 +0700

    я сломал и починил

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ bot = commands.Bot(command_prefix=prefix, intents=intents)
 @bot.command()
 async def load(ctx, extension):
     if ctx.author.id == int(os.environ["BOT_OWNER"]):
-        bot.load_extension(f"cogs.{extension}")
+        bot.load_extension(f"commands.{extension}")
         await ctx.send("Бот загружен")
     else:
         await ctx.send("Загрузить бота может только лягушка")
@@ -26,7 +26,7 @@ async def load(ctx, extension):
 @bot.command()
 async def unload(ctx, extension):
     if ctx.author.id == int(os.environ["BOT_OWNER"]):
-        bot.unload_extension(f"cogs.{extension}")
+        bot.unload_extension(f"commands.{extension}")
         await ctx.send("Бот выгружен")
     else:
         await ctx.send("Выгрузить бота может только лягушка")
@@ -34,14 +34,14 @@ async def unload(ctx, extension):
 @bot.command()
 async def reload(ctx, extension):
     if ctx.author.id == int(os.environ["BOT_OWNER"]):
-        bot.unload_extension(f"cogs.{extension}")
-        bot.load_extension(f"cogs.{extension}")
+        bot.unload_extension(f"commands.{extension}")
+        bot.load_extension(f"commands.{extension}")
         await ctx.send("Бот перезагружен")
     else:
         await ctx.send("Перезагрузить бота может только лягушка")
 
 for filename in os.listdir("./commands"):
     if filename.endswith(".py"):
-        bot.load_extension(f"cogs.{filename[:-3]}")
+        bot.load_extension(f"commands.{filename[:-3]}")
 
 bot.run(os.environ["TOKEN"])
